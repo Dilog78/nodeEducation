b0cea37ac6bd736201aa2c0dccb0fc090ca4c146
Dilog78 <sport777life@gmail.com>
2022-03-18 22:18:50 +0200

    nest

---
+++ src/ormconfig.ts
@@ -0,0 +1,13 @@
+import { ConnectionOptions } from "typeorm";
+
+const config: ConnectionOptions = {
+    type: 'postgres',
+    host:'localhost',
+    port: 5432,
+    username: 'medium',
+    password: 'root',
+    database: 'mediumclone',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+};
+export default config;
