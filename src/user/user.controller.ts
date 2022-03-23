3c3d75545100b9aeac91ace09885160658a049a0
Dilog78 <sport777life@gmail.com>
2022-03-23 22:31:04 +0200

    login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UsePipes, ValidationPipe } from "@nestjs/common";
+import { LoginUserDto } from "./dto/login.dto";
 import { UserDto } from "./dto/user.dto";
+import { ExpressRequestInterface } from "@app/types/expressRequest.interface";
 import { UserResponseInterface } from "./types/userRes.interface";
 import { UserService } from "./user.service";
 
@@ -12,5 +14,17 @@ export class UserController{
     async createUser(@Body('user') createUserDto: UserDto):Promise<UserResponseInterface>{
          const user = await this.userService.createUser(createUserDto);
          return this.userService.userResponse(user);
+    }
+
+    @Post('users/login')
+    @UsePipes(new ValidationPipe())
+    async login(@Body('user') loginDto: LoginUserDto): Promise<UserResponseInterface>{
+        const user = await this.userService.login(loginDto);
+        return this.userService.userResponse(user);
+    }
+
+    @Get('user')
+    async currentUser(@Req() request: ExpressRequestInterface): Promise<UserResponseInterface> {
+        return this.userService.userResponse(request.user);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,9 +3,11 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { UserDto } from "./dto/user.dto";
 import { UserEntity } from "./user.entity";
-import {sign} from "jsonwebtoken";
+import { sign } from "jsonwebtoken";
 import { JWT_SECRET } from "@app/config";
 import { UserResponseInterface } from "./types/userRes.interface";
+import { compare } from 'bcrypt';
+import { LoginUserDto } from "./dto/login.dto";
 
 
 @Injectable()
@@ -14,6 +16,33 @@ export class UserService {
         @InjectRepository(UserEntity)
         private readonly userRepository: Repository<UserEntity>
     ){}
+
+    async findById(id: number): Promise<UserEntity> {
+        return this.userRepository.findOne(id);
+    }
+
+    async login(userDto: LoginUserDto): Promise<UserEntity> {
+        const user = await this.userRepository.findOne(
+            {email: userDto.email},
+            {select: ['id','email','username','bio','image','password']},
+            
+        );
+
+        if(!user){
+            throw new HttpException('Credentials are not valid', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        const checkPass = await compare(userDto.password, user.password);
+
+        if(!checkPass) {
+            throw new HttpException('Credentials are not valid', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        delete user.password;
+
+        return user;
+    }
+
     async createUser(UserDto: UserDto): Promise<UserEntity> {
         const userByEmail = await this.userRepository.findOne({email: UserDto.email});
         const userByUsername = await this.userRepository.findOne({ username: UserDto.username});
@@ -33,7 +62,7 @@ export class UserService {
             email: user.email,
         }, 
         JWT_SECRET
-      );
+      );  
     }
 
     userResponse(user: UserEntity): UserResponseInterface {
